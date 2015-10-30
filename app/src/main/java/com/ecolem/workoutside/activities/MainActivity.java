187b7c039bf090001db9a78a9dabc62b9e31015b
Sandra <snaboulet@modulotech.fr>
2015-10-30 23:45:02 +0100

    Home page

--- app/src/main/java/com/ecolem/workoutside/activities/MainActivity.java
+++ app/src/main/java/com/ecolem/workoutside/activities/MainActivity.java
@@ -1,21 +1,25 @@
 package com.ecolem.workoutside.activities;
 
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.ecolem.workoutside.R;
 import com.ecolem.workoutside.WorkoutSide;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private RelativeLayout mCatalogMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -24,6 +28,21 @@ public class MainActivity extends ActionBarActivity {
         actionbar.setTitle(getResources().getString(R.string.action_bar_title));
         actionbar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.primary)));
 
+        mCatalogMenuButton = (RelativeLayout) findViewById(R.id.menu_catalog);
+        mCatalogMenuButton.setOnClickListener(this);
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.menu_catalog:
+                Intent intent = new Intent(MainActivity.this, MovementActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
 }
