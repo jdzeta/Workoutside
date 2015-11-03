defc7fd44b5e051bde95737975a8af426767251b
Sandra <snaboulet@modulotech.fr>
2015-11-03 11:35:19 +0100

    Firebase logout

--- app/src/main/java/com/ecolem/workoutside/activities/MainActivity.java
+++ app/src/main/java/com/ecolem/workoutside/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ecolem.workoutside.activities;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -10,12 +12,14 @@ import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.ecolem.workoutside.R;
+import com.ecolem.workoutside.manager.UserManager;
 import com.firebase.client.Firebase;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     private RelativeLayout mCatalogMenuButton;
+    private RelativeLayout mLogoutMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +35,50 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
         mCatalogMenuButton = (RelativeLayout) findViewById(R.id.menu_catalog);
         mCatalogMenuButton.setOnClickListener(this);
+        mLogoutMenuButton = (RelativeLayout) findViewById(R.id.menu_logout);
+        mLogoutMenuButton.setOnClickListener(this);
 
 
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.menu_catalog:
                 Intent intent = new Intent(MainActivity.this, CatalogActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.menu_logout:
+                showLogoutAlert();
+                break;
             default:
                 break;
         }
+    }
+
+    private void showLogoutAlert() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        // set title
+        alertDialogBuilder.setTitle(getResources().getString(R.string.menu_logout));
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage(getResources().getString(R.string.logout_alert))
+                .setCancelable(false)
+                .setPositiveButton(getResources().getString(R.string.leave), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        UserManager.getInstance().logout();
+                        MainActivity.this.finish();
+                    }
+                })
+                .setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
