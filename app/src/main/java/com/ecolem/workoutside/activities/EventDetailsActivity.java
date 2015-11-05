2c0dc5fea288787f0d3b37d4ca2c3eb494030f0e
Sandra Naboulet <naboul_s@etna-alternance.net>
2015-11-06 00:53:33 +0100

    Design for event details + list

--- app/src/main/java/com/ecolem/workoutside/activities/EventDetailsActivity.java
+++ app/src/main/java/com/ecolem/workoutside/activities/EventDetailsActivity.java
@@ -1,8 +1,10 @@
 package com.ecolem.workoutside.activities;
 
+import android.graphics.drawable.ColorDrawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -10,6 +12,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.ecolem.workoutside.R;
+import com.ecolem.workoutside.helpers.GeolocHelper;
+import com.ecolem.workoutside.helpers.TimeHelper;
 import com.ecolem.workoutside.manager.EventManager;
 import com.ecolem.workoutside.model.Event;
 import com.ecolem.workoutside.model.User;
@@ -33,29 +37,29 @@ public class EventDetailsActivity extends ActionBarActivity implements EventMana
     private TextView event_detail_creator;
     private TextView event_detail_name;
     private TextView event_detail_min_level;
-    private TextView event_detail_datetime;
+    private TextView event_detail_date;
+    private TextView event_detail_hour;
     private TextView event_detail_location;
+    private TextView event_detail_description;
 
-    private LinearLayout event_detail_buttons;
     private Button event_detail_button_participate;
-    private Button event_detail_button_desistate;
+
 
     private TextView event_detail_nb_participants;
     private ListView event_detail_participant_list;
+
+
+    private ActionBar mActionBar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
 
-        /* Setting tempEvent data
-        String name = "My Event";
-        Date date = Calendar.getInstance(Locale.FRANCE).getTime();
-        String description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus distinctio omnis inventore, quidem magnam eaque voluptates optio cum ullam autem natus nisi mollitia quae deleniti, ab fugiat iusto itaque. Minus.";
-        Integer minLevel = 1;
-        Integer maxParticipants = 3;
-        User creator = UserManager.getInstance().getUser();
-        this.myEvent = new Event(name, date, 12.3, -0.3, description, minLevel, maxParticipants, creator);*/
+        // init actionbar
+        mActionBar = getSupportActionBar();
+        mActionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.primary)));
+        mActionBar.setDisplayHomeAsUpEnabled(true);
 
         // Getting selected event
         Bundle bundle = getIntent().getExtras();
@@ -64,56 +68,51 @@ public class EventDetailsActivity extends ActionBarActivity implements EventMana
         eventManager.getEvent(evUuid, this);
 
         // INIT EVENT DETAILS
-        this.event_detail_creator  = (TextView) findViewById(R.id.event_detail_creator);
-        this.event_detail_name  = (TextView) findViewById(R.id.event_detail_name);
-        this.event_detail_min_level  = (TextView) findViewById(R.id.event_detail_min_level);
-        this.event_detail_datetime  = (TextView) findViewById(R.id.event_detail_datetime);
-        this.event_detail_location  = (TextView) findViewById(R.id.event_detail_location);
-        this.event_detail_nb_participants  = (TextView) findViewById(R.id.event_detail_nb_participants);
+        this.event_detail_creator = (TextView) findViewById(R.id.event_detail_creator);
+        this.event_detail_name = (TextView) findViewById(R.id.event_detail_name);
+        this.event_detail_min_level = (TextView) findViewById(R.id.event_detail_min_level);
+        this.event_detail_date = (TextView) findViewById(R.id.event_detail_date);
+        this.event_detail_hour = (TextView) findViewById(R.id.event_detail_hour);
+        this.event_detail_location = (TextView) findViewById(R.id.event_detail_location);
+        this.event_detail_description = (TextView) findViewById(R.id.event_details_description);
+        this.event_detail_nb_participants = (TextView) findViewById(R.id.event_detail_nb_participants);
         this.event_detail_participant_list = (ListView) findViewById(R.id.event_detail_participant_list);
 
     }
 
     public void settingViews() {
+
         this.event_detail_creator.setText(this.myEvent.getCreator().getFirstname() + " " + this.myEvent.getCreator().getLastname());
         this.event_detail_name.setText(this.myEvent.getName());
+        this.event_detail_description.setText(this.myEvent.getDescription());
+
         // Defining min level
-        String minLevel;
-        switch (this.myEvent.getMinLevel()){
+
+        switch (this.myEvent.getMinLevel()) {
             case 0:
-            default:
-                minLevel = "Débutant";
+                event_detail_min_level.setText(getResources().getString(R.string.level_0));
                 break;
             case 1:
-                minLevel = "Intermédiaire";
+                event_detail_min_level.setText(getResources().getString(R.string.level_1));
                 break;
             case 2:
-                minLevel = "Avancé";
+                event_detail_min_level.setText(getResources().getString(R.string.level_2));
                 break;
             case 3:
-                minLevel = "Expert";
+                event_detail_min_level.setText(getResources().getString(R.string.level_3));
+                break;
+            default:
                 break;
         }
-        this.event_detail_min_level.setText(minLevel);
+
 
         // Turning Time to Datetime
-        Date evDate = this.myEvent.getDate();
-        DateFormat dateFormat = DateFormat.getDateInstance();
-        this.event_detail_datetime.setText(dateFormat.format(evDate));
+        this.event_detail_date.setText(TimeHelper.getEventDateStr(this.myEvent.getDate(), true));
+        this.event_detail_hour.setText(TimeHelper.getEventHourStr(this.myEvent.getDate()));
 
         // Turning location to address
-        double latitude = this.myEvent.getLatitude();
-        double longitude = this.myEvent.getLongitude();
-        GeoLocation evLocation = new GeoLocation(latitude, longitude);
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-        List<Address> addresses = null;
-        try {
-            addresses = geocoder.getFromLocation(evLocation.latitude, evLocation.longitude, 1);
-            System.out.println(addresses);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        this.event_detail_location.setText(addresses.get(0).toString());
+        String address = GeolocHelper.getCityFromLatitudeLongitude(this, this.myEvent.getLatitude(), this.myEvent.getLongitude());
+        this.event_detail_location.setText(address);
 
         // Counting participants
         HashMap<Integer, User> listParticipants = this.myEvent.getParticipants();
@@ -124,7 +123,9 @@ public class EventDetailsActivity extends ActionBarActivity implements EventMana
     public void onGetEventSuccess(Event event) {
         this.myEvent = event;
         // Setting event name in actionbar
-        setTitle(this.myEvent.getName());
+
+        mActionBar.setTitle(this.myEvent.getName());
+
         settingViews();
     }
 
